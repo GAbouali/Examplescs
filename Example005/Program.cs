393d2957863f59356d4185fee32ca2a6aee3948f
Your Gomaa <gomaamhmd@gmail.com>
2023-01-15 00:05:21 +0300

    if&else

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("inter your name: ");
+string username = Console.ReadLine();
+
+if(username == "Masha");
+{
+    Console.WriteLine("yraaaa, this is Masha!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+}
