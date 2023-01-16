78b406ece2950a07484d200ab49d95247f767506
Your Gomaa <gomaamhmd@gmail.com>
2023-01-16 22:04:31 +0300

    008magic

---
+++ Example007-magi/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 35, ya = 1,
+    xb = 1, yb = 15,
+    xc = 70, yc = 15;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("*");
