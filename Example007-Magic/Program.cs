fddf2364de6e7777f6995dc11b21b52a13fb84bd
Your Gomaa <gomaamhmd@gmail.com>
2023-01-16 15:56:17 +0300

    007??

--- Example007-Magic/Program.cs
+++ Example007-Magic/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("*");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 40,
+    xc = 40, yc = 40;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("*");
